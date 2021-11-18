1d9e87f8eae17d7816b77a24dd5dea60c2f02a16
failamir <38488480+failamir@users.noreply.github.com>
2021-11-18 17:46:34 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$kon=mysqli_connect("localhost","root","","db_penyakit",3306);
+$kon=mysqli_connect("localhost:3306","fafa","fafa","db_penyakit");
 if(mysqli_connect_errno()){
 	echo "Failed to Connect to Mysql : ".mysqli_connect_error();
 }
